bca379f825e1e4e7661dbe3ba12bd68c1ee8bc1c
Maharshi020 <66055333+Maharshi020@users.noreply.github.com>
2024-04-16 21:36:06 +0530

    Update MxB Exchange screen

--- MC1Demo/src/screens/MxBExchange.tsx
+++ MC1Demo/src/screens/MxBExchange.tsx
@@ -1,71 +1,88 @@
 import React from 'react';
 import { memo } from 'react';
-import { StyleSheet, View, Text, Button, FlatList, Touchable } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  FlatList,
+  Touchable,
+} from 'react-native';
 import { ProductsType } from '../data/Products';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { fetchProducts } from '../lib/api';
 import { useTranslation } from 'react-i18next';
-
+import { Button as PaperButton } from 'react-native-paper';
+import ExpandableListItem from '../components/ExpandableListItem';
 
 const MxBExchange = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const listData = [
-        { id: 1, name: t('ProductsReceived'), action: t('AddReturn') },
-        { id: 2, name: t('ProductsDelivered'), action: t('AddDelivery') }
-    ];
+  const listData = [
+    { id: 1, name: t('ProductsReceived'), action: t('AddReturn') },
+    { id: 2, name: t('ProductsDelivered'), action: t('AddDelivery') },
+  ];
 
-    const renderItem = ({ item }: any) => (
-        <View >
-            <View style={styles.itemHeading}>
-                <Text style={styles.itemName}>{item.name}</Text>
-                <Text style={styles.rightArrow}>⌃</Text>
-            </View>
-            <View style={styles.buttonBox}>
-                <Button title={item.action} color={'#FFF'}/>
-            </View>
-        </View>
-    );
+  const renderItem = ({ item }) => (
+    <View>
+      <ExpandableListItem
+        clickedChildren={<Text style={styles.itemName}>{item.name}</Text>}
+        expandedChildren={
+          <PaperButton mode="contained" style={styles.buttonBox}>
+            {item.action}
+          </PaperButton>
+        }
+      />
+    </View>
+  );
 
-
-    return (
-        <View style={styles.container}>
-            <Text>{t('Total')} {t('Changes')} MxB: $0.0</Text>
-            <FlatList
-                data={listData}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.id.toString()}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textContainer}>
+        {t('Total')} {t('Changes')} MxB: $0.0
+      </Text>
+      <FlatList
+        data={listData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }}></View>}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 20,
-    },
-    itemName: {
-        color: '#fff',
-    },
-    rightArrow: {
-        position: 'absolute',
-        right: 10,
-        top: 2,
-        fontSize: 40,
-        color: '#fff'
-    },
-    itemHeading: {
-        backgroundColor: '#2196F3',
-        padding: 10,
-        marginTop: 20,
-        color: '#FFF'
-    },
-    buttonBox: {
-        backgroundColor: '#2196F3',
-        padding: 5,
-        margin: 10
-    }
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  textContainer: {
+    backgroundColor: 'white',
+    marginHorizontal: 10,
+    marginBottom: 10,
+    paddingVertical: 10,
+    fontSize: 14,
+    paddingLeft: 10,
+  },
+  itemName: {
+    color: '#fff',
+  },
+  rightArrow: {
+    position: 'absolute',
+    right: 10,
+    top: 2,
+    fontSize: 40,
+    color: '#fff',
+  },
+  itemHeading: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    marginTop: 20,
+    color: '#FFF',
+  },
+  buttonBox: {
+    margin: 10,
+  },
 });
 
 export default memo(MxBExchange);
